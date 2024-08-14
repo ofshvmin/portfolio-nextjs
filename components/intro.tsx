52b5a5d6fdc98a9fc6ad792f746b5648267d0230
Daniel Mathews <daniel.g.mathews@gmail.com>
2024-08-14 17:51:45 -0400

    implement context api for selecting active section. implement custom hook to flag section in view. refactor section components with custom hook

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+
 import Image from 'next/image'
 // import meImage from '@/public/me.png'
 import githubme from '@/public/githubme.png'
@@ -9,10 +9,17 @@ import Link from 'next/link'
 import{ BsArrowRight, BsLinkedin } from 'react-icons/bs'
 import{ HiDownload } from 'react-icons/hi'
 import { FaGithubSquare } from 'react-icons/fa'
+import { useSectionInView } from '@/lib/hooks'
 
 export default function Intro() {
+  const { ref } = useSectionInView('Home', 0.5)
+
   return (
-    <section>
+    <section
+    ref={ref}
+      id='home'
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
       <div className='flex items-center justify-center'>
         <div className='relative'>
           <motion.div 
--- components/skills.tsx
+++ components/skills.tsx
@@ -3,7 +3,10 @@ import SectionHeading from './section-heading'
 
 export default function Skills() {
   return (
-    <section>
+    <section
+      id='skills'
+      className='scroll-mt-28'
+    >
     <SectionHeading>Skills</SectionHeading>
     <div>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis dolorem, doloribus magnam ipsum officia corporis asperiores! Nulla porro nobis optio, architecto debitis suscipit quod at maiores deserunt maxime aliquid sapiente!
