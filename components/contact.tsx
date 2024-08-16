8553fbbde26f98c363ba5ba27ad64be09e9a67ac
Daniel Mathews <daniel.g.mathews@gmail.com>
2024-08-16 12:42:49 -0400

    add contact section and styles

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,13 +1,68 @@
+'use client'
+
 import React from 'react'
 import SectionHeading from './section-heading'
+import { useSectionInView } from '@/lib/hooks'
+import { FaPaperPlane } from 'react-icons/fa'
+import { motion } from 'framer-motion'
 
 export default function Contact() {
+  const { ref } = useSectionInView('Contact')
+
   return (
-    <section>
-    <SectionHeading>Contact</SectionHeading>
-    <div>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum modi iure at, blanditiis error distinctio expedita voluptatibus aperiam voluptatem omnis. Quae debitis quaerat mollitia quis alias a inventore! Qui, tempora.
-    </div>
-  </section>
+    <motion.section 
+      id='contact'
+      ref={ref}
+      className='mb-20 sm:mb-28 w-[min(100%,38rem)]'
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+      viewport={{
+        once: true,
+      }}
+    >
+    <SectionHeading>Contact me</SectionHeading>
+    <p className='text-gray-700 -mt-6 dark:text-white/80'>
+      Please contact me directly at{" "}  
+        <a className='underline' href="mailto:daniel.g.mathews@gmail.com">daniel.g.mathews@gmail.com 
+        </a>{" "}
+      or through this form.
+    </p>
+
+    <form 
+      action=""
+      className='mt-10 flex flex-col dark:text-black'
+    >
+      <input 
+        name="senderEmail"
+        type="email" 
+        required
+        maxLength={500}
+        placeholder='Your email'
+        className='h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none'
+      />
+      <textarea 
+        name="" 
+        id=""
+        className='h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none'
+      >
+
+      </textarea>
+      <button 
+        type="submit"
+        className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-[1.15] hover:scale-110 hover:bg-gray-950 active:scale-105'
+      >
+        Submit <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1
+        ' />
+      </button>
+    </form>
+
+  </motion.section >
   )
 }
