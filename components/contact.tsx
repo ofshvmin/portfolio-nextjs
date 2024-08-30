91c083bf362814f88ef25a23ac7be18edd19c703
Daniel Mathews <daniel.g.mathews@gmail.com>
2024-08-30 10:48:59 -0400

    extract contact form submit button to a separate component.  implement conditional styling to display pending status

--- components/contact.tsx
+++ components/contact.tsx
@@ -3,13 +3,15 @@
 import React from 'react'
 import SectionHeading from './section-heading'
 import { useSectionInView } from '@/lib/hooks'
-import { FaPaperPlane } from 'react-icons/fa'
+
 import { motion } from 'framer-motion'
 import { sendEmail } from '@/actions/sendEmail'
+import { useFormStatus } from 'react-dom'
+import SubmitBtn from './submit-btn'
 
 export default function Contact() {
   const { ref } = useSectionInView('Contact')
-  
+  const { pending } = useFormStatus()
 
 
   return (
@@ -42,7 +44,7 @@ export default function Contact() {
       action={async (formData) => {
         await sendEmail(formData)
       }}
-      
+
       className='mt-10 flex flex-col dark:text-black'
     >
       <input 
@@ -63,13 +65,7 @@ export default function Contact() {
       >
 
       </textarea>
-      <button 
-        type="submit"
-        className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-[1.15] hover:scale-110 hover:bg-gray-950 active:scale-105'
-      >
-        Submit <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1
-        ' />
-      </button>
+      <SubmitBtn />
     </form>
 
   </motion.section >
